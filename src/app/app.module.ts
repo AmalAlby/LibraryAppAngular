3e61321b0eb82b6165b5d1cde36738f0840cb4ba
AmalAlby <amalalbyn@gmail.com>
2022-11-24 20:02:11 +0530

    books entry created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookentryComponent } from './bookentry/bookentry.component';
+
+const myroute:Routes=[
+  {
+    path:"",
+    component:BookentryComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookentryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
