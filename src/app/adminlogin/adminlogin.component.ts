5de76afaebb7f67920e8ff11a3f8dd6221dd0848
AmalAlby <amalalbyn@gmail.com>
2022-11-30 11:22:41 +0530

    booksentry api integration added

--- src/app/adminlogin/adminlogin.component.ts
+++ src/app/adminlogin/adminlogin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adminlogin',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./adminlogin.component.css']
 })
 export class AdminloginComponent {
+
+  username=""
+  password=""
+
+  constructor(private route:Router){}
+
+  admlog=()=>{
+
+    let data:any={"username":this.username,"password":this.password}
+    console.log(data)
+
+    if(data.username=="user" && data.password=="12345"){
+      this.route.navigate(["/entry"])
+    }
+    else{
+      alert("invalid input")
+    }
+  }
 
 }
